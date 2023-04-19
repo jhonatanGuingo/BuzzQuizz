f1c4448a91f245b03012695a8d8331cd0fea972b
Philippe <philippe.idalgoprestes@gmail.com>
2023-04-19 20:58:54 -0300

    Philippe: função de coletar quizzes e mostrar completa

--- scripts/script_pag_inicial.js
+++ scripts/script_pag_inicial.js
@@ -9,7 +9,23 @@ function atualizar_quizzes(){
     send.then(sucess);
     send.catch(error);
     function sucess(response){
-        console.log(response.data);
+        console.log("Encontrei os Quizzes gerais");
+        for (let i = 0; i < response.data.length; i++){
+            if (i < 6){
+                let id_quiz = response.data[i].id;
+                let titulo_quiz = response.data[i].title;
+                let image_quiz = response.data[i].image;
+                let create_div_quiz = document.createElement('div');
+                let create_span_div_quiz = document.createElement('span');
+                let style_line = 'background-image: linear-gradient(360deg, rgb(0, 0, 0, 0.8) 2%, rgb(255, 255, 255, 0) 100%), url(' + image_quiz + ')';
+                create_div_quiz.classList.add("todos-quizzes-quizdiv");
+                create_div_quiz.setAttribute("style", style_line);
+                create_span_div_quiz.classList.add("todos-quizzes-conteudo-tituloquiz");
+                create_span_div_quiz.innerHTML = titulo_quiz;
+                create_div_quiz.appendChild(create_span_div_quiz);
+                document.getElementById("todos_quizzes_conteudo").appendChild(create_div_quiz);
+            }
+        }
     }
     function error(response){
         console.log(response.status);
