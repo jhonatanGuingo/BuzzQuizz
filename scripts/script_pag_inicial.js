3215f14776dcb26fe818e45e62f8adb2ff0acde3
Philippe <philippe.idalgoprestes@gmail.com>
2023-04-19 20:37:35 -0300

    v0

--- scripts/script_pag_inicial.js
+++ scripts/script_pag_inicial.js
@@ -1,11 +1,6 @@
-// alterar o layout de Seus quizzes conforme existência de quizzes pessoais. Lembrar de ocultar o layout de seus-quizzes-criar-border e permitir o seus-quizzes-mostrar//
 function atualizar_seus_quizzes(){
+    axios.defaults.headers.common['Authorization'] = 'seuTokenDeAcessoNoHUB';
+    let send = axios.get('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes');
     document.getElementById("seus_quizzes_criar").style.display = "none";
     document.getElementById("seus_quizzes_mostrar").style.display = "flex";
-    document.getElementById("btn_teste").onclick = reatualizar_seus_quizzes;
-}
-function reatualizar_seus_quizzes(){
-    document.getElementById("seus_quizzes_criar").style.display = "flex";
-    document.getElementById("seus_quizzes_mostrar").style.display = "none";
-    document.getElementById("btn_teste").onclick = atualizar_seus_quizzes;
 }
