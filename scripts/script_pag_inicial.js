ffffccb1248b1ac7c663c50a8b453fbe4562f957
Philippe <philippe.idalgoprestes@gmail.com>
2023-04-19 20:48:00 -0300

    Philippe: começo das funções get para os Quizzes do início

--- scripts/script_pag_inicial.js
+++ scripts/script_pag_inicial.js
@@ -1,6 +1,19 @@
-function atualizar_seus_quizzes(){
-    axios.defaults.headers.common['Authorization'] = 'seuTokenDeAcessoNoHUB';
+let token_api = "Ig05OI8F18Lp90ZDISfjWMt8";
+
+atualizar_quizzes();
+
+function atualizar_quizzes(){
+    axios.defaults.headers.common['Authorization'] = token_api;
     let send = axios.get('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes');
+    console.log("Estou atualizando os quizzes");
+    send.then(sucess);
+    send.catch(error);
+    function sucess(response){
+        console.log(response.data);
+    }
+    function error(response){
+        console.log(response.status);
+    }
     document.getElementById("seus_quizzes_criar").style.display = "none";
     document.getElementById("seus_quizzes_mostrar").style.display = "flex";
 }
