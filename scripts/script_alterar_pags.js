51aca1c7bce9fa823a544655b009aa51701c1e0e
samufigh <samufigh21@gmail.com>
2023-04-19 22:36:43 -0300

    Samuel: botão criar-quizz adicionado

--- scripts/script_alterar_pags.js
+++ scripts/script_alterar_pags.js
@@ -2,3 +2,9 @@ function alt_pag_inicial_quiz(){
     document.getElementById("pag_inicial").style.display = "none";
     document.getElementById("pag_inicial").style.display = "flex";
 }
+function altCriarQuizz(){
+    let=pagInicial = document.getElementById("pag_inicial"); 
+    pagInicial.classList.add('esconder');
+    let info = document.querySelector('.info-do-quizz');
+    info.classList.remove('esconder');
+}
