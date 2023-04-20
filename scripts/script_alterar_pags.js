faa8fd6a8db0cc9575807eb346af83e275153e69
Philippe <philippe.idalgoprestes@gmail.com>
2023-04-19 22:04:05 -0300

    Philippe: funções da página inicial completas

--- scripts/script_alterar_pags.js
+++ scripts/script_alterar_pags.js
@@ -1,4 +1,15 @@
-function alt_pag_inicial_quiz(){
-    document.getElementById("pag_inicial").style.display = "none";
-    document.getElementById("pag_inicial").style.display = "flex";
+function alt_paginic_quiz(){
+    let id = event.target.id;
+    let send = axios.get("https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes/" + id);
+    send.then(sucess);
+    send.catch(error);
+    function sucess(response){
+        let id_quiz = response.data.id;
+        let titulo_quiz = response.data.title;
+        let image_quiz = response.data.image
+        console.log(response.data);
+    }
+    function error(response){
+        console.log(response.status);
+    }
 }
--- scripts/script_pag_inicial.js
+++ scripts/script_pag_inicial.js
@@ -1,4 +1,7 @@
 let token_api = "Ig05OI8F18Lp90ZDISfjWMt8";
+localStorage.setItem("ids_quizzes_criados", "[]");
+let user_quizzes = JSON.parse(localStorage.getItem("ids_quizzes_criados"));
+console.log(user_quizzes);
 
 atualizar_quizzes();
 
@@ -11,25 +14,50 @@ function atualizar_quizzes(){
     function sucess(response){
         console.log("Encontrei os Quizzes gerais");
         for (let i = 0; i < response.data.length; i++){
-            if (i < 6){
-                let id_quiz = response.data[i].id;
-                let titulo_quiz = response.data[i].title;
-                let image_quiz = response.data[i].image;
-                let create_div_quiz = document.createElement('div');
-                let create_span_div_quiz = document.createElement('span');
-                let style_line = 'background-image: linear-gradient(360deg, rgb(0, 0, 0, 0.8) 2%, rgb(255, 255, 255, 0) 100%), url(' + image_quiz + ')';
+            let id_quiz = response.data[i].id;
+            let titulo_quiz = response.data[i].title;
+            let image_quiz = response.data[i].image;
+            let create_btn_quiz = document.createElement('button');
+            let create_div_quiz = document.createElement('div');
+            let create_span_div_quiz = document.createElement('span');
+            let style_line = 'background-image: linear-gradient(360deg, rgb(0, 0, 0, 0.8) 2%, rgb(255, 255, 255, 0) 100%), url(' + image_quiz + ')';
+            let a = 0;
+            for (let j = 0; j < user_quizzes.length; j++){
+                if (user_quizzes[j] == id_quiz){
+                    a = 1;
+                }
+            }
+            if (a == 0){
+                create_btn_quiz.classList.add('button-quiz');
+                create_btn_quiz.onclick = alt_paginic_quiz;
                 create_div_quiz.classList.add("todos-quizzes-quizdiv");
+                create_div_quiz.id = id_quiz;
                 create_div_quiz.setAttribute("style", style_line);
                 create_span_div_quiz.classList.add("todos-quizzes-conteudo-tituloquiz");
                 create_span_div_quiz.innerHTML = titulo_quiz;
                 create_div_quiz.appendChild(create_span_div_quiz);
-                document.getElementById("todos_quizzes_conteudo").appendChild(create_div_quiz);
+                create_btn_quiz.appendChild(create_div_quiz);
+                document.getElementById("todos_quizzes_conteudo").appendChild(create_btn_quiz);
             }
+            else{
+                create_btn_quiz.classList.add('button-quiz');
+                create_btn_quiz.onclick = alt_paginic_quiz;
+                create_div_quiz.classList.add("seus-quizzes-mostrar-quizdiv");
+                create_div_quiz.id = id_quiz;
+                create_div_quiz.setAttribute("style", style_line);
+                create_span_div_quiz.classList.add("seus-quizzes-mostrar-conteudo-tituloquiz");
+                create_span_div_quiz.innerHTML = titulo_quiz;
+                create_div_quiz.appendChild(create_span_div_quiz);
+                create_btn_quiz.appendChild(create_div_quiz);
+                document.getElementById("seus_quizzes_mostrar_conteudo").appendChild(create_btn_quiz);
+            }
+        }
+        if (user_quizzes.length != 0){
+            document.getElementById("seus_quizzes_mostrar").style.display = "flex";
+            document.getElementById("seus_quizzes_criar").style.display = "none";
         }
     }
     function error(response){
         console.log(response.status);
     }
-    document.getElementById("seus_quizzes_criar").style.display = "none";
-    document.getElementById("seus_quizzes_mostrar").style.display = "flex";
 }
