e755bcdd050ad193e54eeef8d791c3b0547f362a
samufigh <samufigh21@gmail.com>
2023-04-20 01:23:08 -0300

    Samuel: att informações básicas do quizz

--- scripts/script_alterar_pags.js
+++ scripts/script_alterar_pags.js
@@ -5,20 +5,21 @@ function alt_pag_inicial_quiz(){
 //muda para a tela de criação de quizz
 function altCriarQuizz(){
     let=pagInicial = document.getElementById("pag_inicial"); 
+    console.log(pagInicial);
     pagInicial.classList.add('esconder');
-    let info = document.querySelector('.info-do-quizz');
-    info.classList.remove('esconder');
+    console.log(pagInicial);
+    let info = document.getElementById('info_do_quizz');
+    console.log(info);
+    info.classList.add('info-do-quizz');
     console.log(info);
 }
 //muda para tela de criação de perguntas
 function altCriarPerguntas(){
     //alert('oii');
-    let=criarQuizz = document.getElementById("info_do_quizz");
-    console.log(criarQuizz);
-    criarQuizz.classList.add('esconder');
-    console.log(criarQuizz);
-    let perguntas = document.querySelector('.perguntas-do-quizz');
-    perguntas.classList.remove('esconder');
+    let=criarQuizz = document.classList('.info-do-quizz') ;
+    criarQuizz.classList.remove('info-do-quiz');
+    let perguntas = document.getElementById('perguntas_do_quizz');
+    perguntas.classList.add('perguntas-do-quizz');
 }
 
 function alt_paginic_quiz(){
